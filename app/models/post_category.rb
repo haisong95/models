b0e15131a8b2dce1be4d1f7833aa2624af23eaa4
Haisong Huang <Haisong@nat-10-22-93-195.uofm.wireless.umn.edu>
2014-01-10 20:00:04 -0600

    a

---
+++ app/models/post_category.rb
@@ -0,0 +1,3 @@
+class PostCategory < ActiveRecord::Base
+	belongs_to :post
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '47959b762471dff328e56384ffd9ea0a86b1d9eb5f77cb1cb1bc8f30805769aa176c4cce194060636479beb93bf88b93a1ff257116f1b950e3a18977c4a3b3c0'
